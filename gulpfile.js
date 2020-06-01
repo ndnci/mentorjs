78565519e6f1d40c4baa981389b448cf215d5351
NDNCI <contact@ndnci.com>
2020-06-01 22:09:56 +0200

    gulp config and assets folder/files added

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+// compile scss into css
+function style() {
+    // 1. where is my scss file
+    return gulp.src('./assets/scss/**/*.scss')
+    // 2. pass that file through sass compiler
+        .pipe(sass())
+    // 3. where do i save the compiled CSS ?
+        .pipe(gulp.dest('./dist/css'))
+        // 4. stream changes to all browser
+        .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('./assets/scss/**/*.scss', style);
+    gulp.watch('./**/*.html').on('change', browserSync.reload);
+    gulp.watch('./assets/js/**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
