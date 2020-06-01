a72a84853994bd74116d3ccf71734617fc7edab5
NDNCI <contact@ndnci.com>
2020-06-01 23:06:03 +0200

    gulp config changed and babel added

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,10 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
 const browserSync = require('browser-sync').create();
 
-// compile scss into css
+// sass compiler
 function style() {
     // 1. where is my scss file
     return gulp.src('./assets/scss/**/*.scss')
@@ -22,6 +23,27 @@ function style() {
         .pipe(browserSync.stream());
 }
 
+// babel compiler
+function javascript() {
+    // 1. where is my js file
+    return gulp.src('./assets/js/**/*.js')
+    // 2 init sourcemaps
+        .pipe(sourcemaps.init())
+    // 2.1 pass that file through babel compiler
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+    // 2.2 concat files with custom name
+        .pipe(concat('mentor.js'))
+    // 2.3 write sourcemaps file
+        .pipe(sourcemaps.write())
+    // 3. where do i save the compiled JS ?
+        .pipe(gulp.dest('./dist/js'))
+    // 4. stream changes to all browser
+        .pipe(browserSync.stream());
+}
+
+// gulp watcher
 function watch() {
     browserSync.init({
         server: {
@@ -31,10 +53,16 @@ function watch() {
     // listening all scss files in assets/scss directory
     gulp.watch('./assets/scss/**/*.scss', style);
     // listening all js files in assets/js directory
-    gulp.watch('./assets/js/**/*.js').on('change', browserSync.reload);
+    gulp.watch('./assets/js/**/*.js', javascript);
     // listening all html files in root directory
     gulp.watch('./**/*.html').on('change', browserSync.reload);
 }
 
+// default gulp function for using only "gulp" on terminal
+gulp.task('default', () =>
+    watch()
+);
+
 exports.style = style;
+exports.javascript = javascript;
 exports.watch = watch;
