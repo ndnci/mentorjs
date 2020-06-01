bcd4189b85c8954fd003701e6a1ee04dce29a0af
NDNCI <contact@ndnci.com>
2020-06-01 22:43:23 +0200

    gulp config changed and gulp sourcemaps added

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,21 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
 
 // compile scss into css
 function style() {
     // 1. where is my scss file
     return gulp.src('./assets/scss/**/*.scss')
-    // 2. pass that file through sass compiler
+    // 2 init sourcemaps
+        .pipe(sourcemaps.init())
+    // 2.1 pass that file through sass compiler
         .pipe(sass())
-    // 2.1 concat files
+    // 2.2 concat files with custom name
         .pipe(concat('mentor.css'))
+    // 2.3 write sourcemaps file
+        .pipe(sourcemaps.write())
     // 3. where do i save the compiled CSS ?
         .pipe(gulp.dest('./dist/css'))
     // 4. stream changes to all browser
@@ -23,9 +28,12 @@ function watch() {
             baseDir: './'
         }
     });
+    // listening all scss files in assets/scss directory
     gulp.watch('./assets/scss/**/*.scss', style);
-    gulp.watch('./**/*.html').on('change', browserSync.reload);
+    // listening all js files in assets/js directory
     gulp.watch('./assets/js/**/*.js').on('change', browserSync.reload);
+    // listening all html files in root directory
+    gulp.watch('./**/*.html').on('change', browserSync.reload);
 }
 
 exports.style = style;
